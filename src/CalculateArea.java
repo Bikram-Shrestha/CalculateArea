b84af5c927c0d9a3ab4aa4be13a3a35ecb072df6
Bikram-Shrestha <bikramshrestha777@gmail.com>
2018-03-04 18:57:42 +1100

    Added exception handling for the user input while selecting provided options.

--- src/CalculateArea.java
+++ src/CalculateArea.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /*This is the main class and will provide an option
 * for the user to choose what action user want to
 * perform and allow user to choose whether user want to
@@ -6,7 +7,7 @@ import java.util.Scanner;
 
 public class CalculateArea {
     public static void main (String[] args){
-        int optionSelection;
+        String option;
         System.out.println("This application will calculate area.\n" +
                 "Please select one of the options to calculate area.\n" +
                 "Option     \t Enter    \n" +
@@ -17,34 +18,49 @@ public class CalculateArea {
                 "Please enter one of the number to select the option\n" +
                 " or enter any other value to exit. ");
         Scanner optionScan = new Scanner(System.in);
-        optionSelection    = optionScan.nextInt();
+        option    = optionScan.nextLine();
+        try {
+            int optionSelection = Integer.parseInt(option);
 
-        // use switch to show options to select the required geometry for area calculation.
-        switch (optionSelection) {
-            case 1:
-            System.out.println("You have selected to calculate area of Circle.");
-            CircleArea.circleArea();
+
+            System.out.println(optionSelection);
+
+            // use switch to show options to select the required geometry for area calculation.
+
+            switch (optionSelection) {
+                case 1:
+                    System.out.println("You have selected to calculate area of Circle.");
+                    CircleArea.circleArea();
                     break;
 
-            case 2:
-            System.out.println("You have selected to calculate area of Rectangle.");
-            RectangleArea.rectangleArea();
+                case 2:
+                    System.out.println("You have selected to calculate area of Rectangle.");
+                    RectangleArea.rectangleArea();
                     break;
-            case 3:
-            System.out.println("You have selected to calculate area of Square.");
-            SquareArea.squareArea();
-                    break;
-
-            case 4:
-            System.out.println("You have selected to calculate area of Triangle.");
-            TriangleArea.triangleArea();
+                case 3:
+                    System.out.println("You have selected to calculate area of Square.");
+                    SquareArea.squareArea();
                     break;
 
-            default:
-            System.out.println("You have selected not to calculate area.\n" +
-                    "Thank you for trying out the application");
+                case 4:
+                    System.out.println("You have selected to calculate area of Triangle.");
+                    TriangleArea.triangleArea();
+                    break;
+
+                default:
+                    System.out.println("You have selected not to calculate area.\n" +
+                            "Thank you for trying out the application");
+                    System.exit(0);
+                    break;
+            }
+
+        }
+
+        catch (NumberFormatException nfe)
+        {
+            System.out.println("You have entered other than option provided.\n" +
+                    "This application is going to close.");
             System.exit(0);
-                    break;
         }
 
 
