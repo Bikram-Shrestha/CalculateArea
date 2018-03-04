bc4272c404d9d2fb9927f1399fc439f07bca7a74
Bikram Shrestha <30488716+Bikram-Shrestha@users.noreply.github.com>
2018-03-04 21:10:43 +1100

    Exception handling.

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -1,6 +1,8 @@
 import java.lang.Math;
 import java.text.DecimalFormat;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /* This class contain circleArea method which can take radius and calculate area of circle.
 * This class also use math class to get the value of pi*/
@@ -11,12 +13,17 @@ public class CircleArea{
     double PI = Math.PI; //Assigning the value of PI from the Math class.
     System.out.println("Please enter the radius of the Circle.");
     Scanner radiusInput = new Scanner(System.in);
+    try{
     double radius = radiusInput.nextDouble();
     double areaCircle = PI * (radius * radius);
 
     System.out.println("The area of the Circle is "+
                         (new DecimalFormat("##.##").format(areaCircle)) +
                         " square meter.");
+    }
+    catch (InputMismatchException ime){
+        System.out.println("Please enter the value in correct format. Example 5");
+    }
     }
 
 }
